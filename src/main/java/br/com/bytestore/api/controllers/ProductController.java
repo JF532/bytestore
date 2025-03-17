2b5da1d63331ed0507bcbc1aa431b534125d37e3
JF532 <jfilipe532@hotmail.com>
2025-03-16 21:23:10 -0300

    Criação do banco de dados

--- src/main/java/br/com/bytestore/api/controllers/ProductController.java
+++ src/main/java/br/com/bytestore/api/controllers/ProductController.java
@@ -1,7 +1,14 @@
 package br.com.bytestore.api.controllers;
 
+import java.util.List;
+
 import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +22,16 @@ public class ProductController {
 
 	@Autowired
 	ProductService productService;
+	
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
 
-	public void createProduct(Product product) {
-
-		productService.store(product);
+		return new ResponseEntity<>(productService.store(product), HttpStatus.CREATED);
 	}
-
+	
+	@GetMapping
+	public List<Product> listAll()
+	{
+		return productService.getAll();
+		}
 }
